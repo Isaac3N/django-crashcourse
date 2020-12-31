5394641a90959d263b8bcbf095f8f078dfd7eaee
Isaac3N <theisaac3n@gmail.com>
2020-12-31 13:47:19 +0100

    django crashcourse

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ISAAC3N\PycharmProjects\django crashcourse\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
